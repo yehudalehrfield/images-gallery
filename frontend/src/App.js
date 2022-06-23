ba9a43991b3fb70b618c06263f2196a20ecedd0e
Yehuda Lehrfield <yll2113@columbia.edu>
2022-06-23 15:27:05 -0400

    get saved imaged to frontend from mongo db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
@@ -13,18 +13,21 @@ const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
 
+  const getSavedImages = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/images`);
+      setImages(res.data || []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // call useEffect which calls getSavedImages;
+  // second argument is the dependancy such that this is only called once (when App renders)
+  useEffect(() => getSavedImages(), []);
+
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    // console.log('sending fetch request');
-    // fetch(`${API_URL}/new-image?query=${word}`)
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log('adding found image to the state');
-    //     setImages([{ ...data, title: word }, ...images]);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
 
     try {
       const res = await axios.get(`${API_URL}/new-image?query=${word}`);
